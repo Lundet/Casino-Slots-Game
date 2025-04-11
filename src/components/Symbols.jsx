8b13d90e495244e6d56eeb231b071d751838e994
Lundet <hannes.j.lundin@gmail.com>
2025-04-11 15:46:24 +0200

    Change payouttable

--- src/components/Symbols.jsx
+++ src/components/Symbols.jsx
@@ -1,9 +1,8 @@
-// No need for return here, direct export works
 export const symbolPayouts = {
-    arrow: [0, 0, 2, 4, 8, 12],
     silvercoin: [0, 0, 2, 4, 8, 12],
-    wall: [0, 0, 4, 8, 12, 20],
-    shield: [0, 0, 3, 5, 10, 25],
+    shield: [0, 0, 2, 4, 8, 12],
+    arrow: [0, 0, 3, 8, 10, 20],
+    wall: [0, 0, 4, 5, 12, 25],
     spearman: [0, 0, 6, 12, 20, 75],
     archer: [0, 0, 6, 12, 20, 150],
     knight: [0, 0, 10, 15, 30, 250],
@@ -12,13 +11,12 @@ export const symbolPayouts = {
     king: [0, 0, 15, 50, 500, 1000],
     wild: [0, 0, 20, 50, 500, 1000],
     bonus: [0, 0, 10, 20, 30, 40],
-};
 
-export const symbolProbabilities = {
-    arrow: 15,
+};export const symbolProbabilities = {
     silvercoin: 15,
+    shield: 15,
+    arrow: 13,
     wall: 13,
-    shield: 13,
     spearman: 10,
     archer: 8,
     knight: 7,
@@ -28,3 +26,4 @@ export const symbolProbabilities = {
     bonus: 10,
     wild: 3,
 };
+
